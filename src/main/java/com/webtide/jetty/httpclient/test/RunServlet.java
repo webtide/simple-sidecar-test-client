b853cf94c21e889c3a798da680fa0fa398a17baa
Olivier Lamy <olamy@apache.org>
2023-01-25 17:22:10 +1000

    do not wait for the end
    Signed-off-by: Olivier Lamy <olamy@apache.org>

--- src/main/java/com/webtide/jetty/httpclient/test/RunServlet.java
+++ src/main/java/com/webtide/jetty/httpclient/test/RunServlet.java
@@ -115,7 +115,7 @@ public class RunServlet extends HttpServlet {
             Histogram histogram = responseTimeListener.histogram;
             LOGGER.info(new HistogramSnapshot(histogram).toString());
         });
-        complete.join();
+        //complete.join();
         //complete.get(minutes + 10, TimeUnit.MINUTES);
 
     }
